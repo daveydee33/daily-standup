8ead2bd58f412d94032f663fe0d974a4a95e348a
Dave Degeatano <davey.dee33@gmail.com>
2019-11-14 10:08:43 +0700

    some cleanup

--- dsItem.js
+++ dsItem.js
@@ -1,21 +1,3 @@
-// TODO:
-// -- add a launch script somewhere?  ~/bin/dsItem
-// -- optionally pass these values on command line - pass URL as first argument and skip the propmt, or pre-fill it
-// -- error if no URL or desc provided (or default like TITLE)
-// -- move this script (app?) to my bin directory or something
-// -- prompt to ask for another?
-// -- copy to clipboard - like pbcopy
-// -- have default colors already
-// -- fetch the title from the URL (but I can't do this because JIRA is behind a login/session)
-// -- emoji or something fun
-// -- make a UI for it and put it on Netlify
-// -- put it in Github
-
-// Sample values
-// const url = "https://pm.axa-asia.com/browse/SGIAASCM-999";
-// const desc = "[SLASH][SmartPlan, SmartCare] Check document template to not have calculations, should only be read-only"
-// const status = "In Progress";
-
 const inquirer = require("inquirer");
 
 const doIt = values => {
@@ -23,7 +5,6 @@ const doIt = values => {
   const id = url.split("/").pop();
   const descEscaped = desc.replace(/\[/g, "\\[").replace(/\]/g, "\\]");
   const statusMacro = `{status:colour=${color}|title=${status}}`;
-  //   const statusMacro = `{status:colour=Gray|title=${status}|subtle=false}`;  // status macro -- https://confluence.atlassian.com/doc/status-macro-223222355.html
 
   console.log(`
 In Confluence, insert Markup with Insert > Markup (Cmd + Shift + d).
