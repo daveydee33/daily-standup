68618a5bdc6d56630c6e3a028fabca928d8ec4d3
Dave Degeatano <davey.dee33@gmail.com>
2019-11-15 15:33:45 +0700

    change to fetch the description - but need to provide JSESSIONID from browser

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # daily-standup
+
 Node command line utility to put our daily standup task and status into Confluence Wiki format.
 
 # Usage
+
 Run the CLI app
+
 ```
 node dsItem.js
 ```
 
-Copy and paste the output into Confluence page Wiki Markup editor.  
+Copy and paste the output into Confluence page Wiki Markup editor.
 
 On a Confluence page, click to Edit the page, and then Insert > Markup (or Cmd+Shift+d) and paste the output from the command.
 
 # Dependencies
-* [Inquirer](https://www.npmjs.com/package/inquirer) for nicer command line input
+
+- [Inquirer](https://github.com/SBoudrias/Inquirer.js) for nicer command line input
+- [Axios](https://github.com/axios/axios) for fetching titles
--- dsItem.js
+++ dsItem.js
@@ -1,36 +1,66 @@
 const inquirer = require("inquirer");
+const https = require("https");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const doIt = values => {
-  const { url, desc, status, color } = values;
-  const id = url.split("/").pop();
+// should move this to a config file
+const URL_PREFIX = "";
+const JSESSIONID = "";
+// TEMP
+// SGIAASCM-999
+
+const printMarkup = values => {
+  const { storyId, desc, status, color } = values;
+  const url = `${URL_PREFIX}${storyId}`;
   const descEscaped = desc.replace(/\[/g, "\\[").replace(/\]/g, "\\]");
   const statusMacro = `{status:colour=${color}|title=${status}}`;
 
   console.log(`
-In Confluence, insert Markup with Insert > Markup (Cmd + Shift + d).
+In Confluence, click to Edit the page (or press E), then insert markup with Insert > Markup (Cmd + Shift + d).  Paste the following as Confluence Wiki Markup.
 
-Confluence Wiki Markup
-
-* [${id}|${url}] ${descEscaped} ${statusMacro}
+-----
+* [${storyId}|${url}] ${descEscaped} ${statusMacro}
+-----
 
 `);
+};
+
+const fetchDescription = async storyId => {
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+  const res = await axios.get(`${URL_PREFIX}${storyId}`, {
+    headers: { Cookie: "JSESSIONID=04FCA4857B516637B7D1E9EC131FB1DC; " },
+    httpsAgent: agent
+  });
+  const html = res.data;
+  const $ = cheerio.load(html);
+  const desc = $("#summary-val").text();
+  return desc;
 };
 
 inquirer
   .prompt([
     {
-      name: "url",
-      message: "URL of the user story from Jira?"
-    },
-    {
-      name: "desc",
-      message: "Description of the user story?"
+      name: "storyId",
+      message: "ID of the user story from Jira?"
     },
     {
       type: "list",
       name: "status",
       message: "What is the status?",
-      choices: ["In Progress", "To Be Done", "Done"],
+      choices: [
+        "In Progress",
+        "PR in Develop",
+        "Develop",
+        "PR in Master",
+        "Done",
+        "To Raise PR in Develop",
+        "To Be in Develop",
+        "To Raise PR in Master",
+        "To Be Done",
+        "OTHER"
+      ],
       default: "In Progress"
     },
     {
@@ -41,6 +71,11 @@ inquirer
       default: "Blue"
     }
   ])
-  .then(values => {
-    doIt(values);
+  .then(async values => {
+    // fetch user story description
+    const desc = await fetchDescription(values.storyId);
+    // add it to the values object
+    values = { ...values, desc };
+    // call function to print it all
+    printMarkup(values);
   });
