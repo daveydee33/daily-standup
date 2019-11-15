d0b62131e627f7e350d6f9ddc11b5031164f6ada
Dave Degeatano <davey.dee33@gmail.com>
2019-11-15 17:23:35 +0700

    take JSESSIONID from Chrome browser cookies

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # daily-standup
 
-Node command line utility to put our daily standup task and status into Confluence Wiki format.
+Node command line utility to put our daily standup task information and status into Confluence Wiki format.
+
+# Setup
+
+- You need to have a successful login to the Jira page from Chrome first.
+- Set the `URL_PREFIX` value in the `dsItem.js` script first. This is the Jira URL - eg: `"https://pm.axa-asia.com/browse/"`
 
 # Usage
 
@@ -10,11 +15,20 @@ Run the CLI app
 node dsItem.js
 ```
 
+Alternatively you can run it with the Jira storyID as the first command line argument. eg:
+
+```
+node dsItem.js SGIAASCM-999
+```
+
 Copy and paste the output into Confluence page Wiki Markup editor.
 
 On a Confluence page, click to Edit the page, and then Insert > Markup (or Cmd+Shift+d) and paste the output from the command.
 
-# Dependencies
+# Tech Used
 
 - [Inquirer](https://github.com/SBoudrias/Inquirer.js) for nicer command line input
-- [Axios](https://github.com/axios/axios) for fetching titles
+- [Axios](https://github.com/axios/axios) for fetching the Jira page by url
+- [Cheerio](https://github.com/cheeriojs/cheerio) for parsing the return HTML for the description text
+- [chrome-cookies-secure](https://github.com/bertrandom/chrome-cookies-secure) to retrieve the JSESSIONID cookie value from Chrome
+- **https** from NodeJS to handle an issue and ignore self-signed SSL cert
--- dsItem.js
+++ dsItem.js
@@ -2,16 +2,28 @@ const inquirer = require("inquirer");
 const https = require("https");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const chrome = require("chrome-cookies-secure");
 
-// should move this to a config file
-const URL_PREFIX = "";
-const JSESSIONID = "";
-// TEMP
-// SGIAASCM-999
+// should move this to a config file?
+const URL_PREFIX = "https://pm.axa-asia.com/browse/";
+
+const getSessionIdFromChromeCookies = url => {
+  return new Promise((resolve, reject) => {
+    chrome.getCookies(url, (err, cookies) => {
+      if (err) {
+        console.error(
+          `Error trying to get JSESSIONID for that URL.  Might need to login from Chrome first? --> ${url}`,
+          err
+        );
+        reject(err);
+      }
+      resolve(cookies.JSESSIONID);
+    });
+  });
+};
 
 const printMarkup = values => {
-  const { storyId, desc, status, color } = values;
-  const url = `${URL_PREFIX}${storyId}`;
+  const { url, storyId, desc, status, color } = values;
   const descEscaped = desc.replace(/\[/g, "\\[").replace(/\]/g, "\\]");
   const statusMacro = `{status:colour=${color}|title=${status}}`;
 
@@ -21,16 +33,15 @@ In Confluence, click to Edit the page (or press E), then insert markup with Inse
 -----
 * [${storyId}|${url}] ${descEscaped} ${statusMacro}
 -----
-
 `);
 };
 
-const fetchDescription = async storyId => {
+const fetchDescriptionFromJiraPage = async (storyId, jSessionId) => {
   const agent = new https.Agent({
     rejectUnauthorized: false
   });
   const res = await axios.get(`${URL_PREFIX}${storyId}`, {
-    headers: { Cookie: "JSESSIONID=04FCA4857B516637B7D1E9EC131FB1DC; " },
+    headers: { Cookie: `JSESSIONID=${jSessionId}` },
     httpsAgent: agent
   });
   const html = res.data;
@@ -39,43 +50,50 @@ const fetchDescription = async storyId => {
   return desc;
 };
 
-inquirer
-  .prompt([
-    {
-      name: "storyId",
-      message: "ID of the user story from Jira?"
-    },
-    {
-      type: "list",
-      name: "status",
-      message: "What is the status?",
-      choices: [
-        "In Progress",
-        "PR in Develop",
-        "Develop",
-        "PR in Master",
-        "Done",
-        "To Raise PR in Develop",
-        "To Be in Develop",
-        "To Raise PR in Master",
-        "To Be Done",
-        "OTHER"
-      ],
-      default: "In Progress"
-    },
-    {
-      type: "list",
-      name: "color",
-      message: "Color of the status indicator",
-      choices: ["Gray", "Blue", "Green", "Red", "Yellow"],
-      default: "Blue"
-    }
-  ])
-  .then(async values => {
-    // fetch user story description
-    const desc = await fetchDescription(values.storyId);
-    // add it to the values object
-    values = { ...values, desc };
-    // call function to print it all
-    printMarkup(values);
-  });
+const questions = [
+  {
+    name: "storyId",
+    message: "ID of the user story from Jira?",
+    default: process.argv[2]
+  },
+  {
+    type: "list",
+    name: "status",
+    message: "What is the status?",
+    choices: [
+      "In Progress",
+      "PR in Develop",
+      "Develop",
+      "PR in Master",
+      "Done",
+      "To Raise PR in Develop",
+      "To Be in Develop",
+      "To Raise PR in Master",
+      "To Be Done",
+      "OTHER"
+    ],
+    default: "In Progress"
+  },
+  {
+    type: "list",
+    name: "color",
+    message: "Color of the status indicator",
+    choices: ["Gray", "Blue", "Green", "Red", "Yellow"],
+    default: "Blue"
+  }
+];
+
+inquirer.prompt(questions).then(async values => {
+  const url = `${URL_PREFIX}${values.storyId}`;
+
+  // fetch the JSESSIONID from Chrome cookies
+  const jSessionId = await getSessionIdFromChromeCookies(url);
+
+  // fetch user story description from Jira url
+  const desc = await fetchDescriptionFromJiraPage(values.storyId, jSessionId);
+
+  values = { ...values, url, desc };
+
+  // call function to print it all
+  printMarkup(values);
+});
